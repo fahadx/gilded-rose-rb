57ba1cc4956de229630366d4060e1dd05ad9c875
Your Name <fahad@goodlogics.com>
2019-05-15 21:21:25 +0500

    restructured code and wrote few tests

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,53 +3,77 @@ class GildedRose
     @items = items
   end
 
+
+  def change_quality_by(item,value)
+    new_value = item.quality+value
+
+
+    #can't be negative
+    if(new_value < 0)
+      new_value = 0
+    end
+
+    #can't be greater then 50
+    if(new_value > 50)
+     new_value = 50
+    end
+
+    item.quality=new_value
+    
+
+
+  end
+
+
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+      
+      if item.name == 'Aged Brie'
+        # 'Aged Brie' actually increases in Quality the older it gets
+        change_quality_by(item,1)
+
+      elsif item.name.downcase.include? 'backstage passes'
+        # 'Backstage passes', like aged brie, increases in Quality as its SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+
+        if item.sell_in <= 0
+          #set quality to zero
+          change_quality_by(item, -1*item.quality)
+        elsif item.sell_in <= 5
+          #increase by 3
+          change_quality_by(item,3)
+        elsif item.sell_in <= 10
+          #increase by 2
+          change_quality_by(item,2)
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          #increase by 1
+          change_quality_by(item,1)
         end
+
+      elsif item.name.downcase.include? 'sulfuras'
+        # 'Sulfuras', being a legendary item, never has to be sold or decreases in Quality
+        # no change in quality
+        # no decrease in sell_in
+
+        item.sell_in += 1 # to cancel the decrease effect for Sulfuras
+      
+      elsif item.name.downcase.include? 'conjured'
+        # 'Conjured' items degrade in Quality twice as fast as normal items
+        
+        change_quality_by(item,-2)
+
+      else
+        # for other items decreate quality by 1
+        
+        change_quality_by(item,-1)
+      
       end
-    end
+      
+      #decreate the sell in value for all
+      item.sell_in = item.sell_in - 1
+
+    end  
   end
+
 end
 
 class Item
--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -2,9 +2,113 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
-  def test_foo
-    items = [Item.new('foo', 0, 0)]
+  
+
+  def test_quality_should_not_be_greater_then_50
+  	items = [
+  				Item.new('Aged Brie', 2, 49),
+  				Item.new('Backstage passes',2,49),
+  				Item.new('Backstage passes',7,49),
+  				Item.new('Backstage passes',12,50),
+  				Item.new('Conjured Mana Cake',3,49)
+  			]
     GildedRose.new(items).update_quality()
-    assert_equal items[0].name, 'fixme'
+    assert items[0].quality <= 50
+    assert items[1].quality <= 50
+    assert items[2].quality <= 50
+    assert items[3].quality <= 50
+    assert items[4].quality <= 50
+
   end
+
+  def test_quality_should_not_be_in_negative
+    items = [
+          Item.new('Elixir of the Mongoose', 2, 0),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality >= 0
+  	
+  end
+
+  def test_aged_brie_quality_should_increment_by_one
+    items = [
+          Item.new('Aged Brie', 2, 0),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 1
+  end
+
+  def test_backstage_passes_quality_should_increment_by_1_if_sell_in_gt_10
+    items = [
+          Item.new('Backstage passes to a TAFKAL80ETC concert', 12, 1),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 2
+  end 
+
+  def test_backstage_passes_quality_should_be_zero_if_sell_in_0_or_less
+    items = [
+          Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 12),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 0
+  end
+
+  def test_backstage_passes_quality_should_increment_by_3_if_sell_in_lt_eq_5
+    items = [
+          Item.new('Backstage passes to a TAFKAL80ETC concert', 3, 12),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 15
+  end
+
+  def test_backstage_passes_quality_should_increment_by_2_if_sell_in_lt_eq_10_and_gt_5
+    items = [
+          Item.new('Backstage passes to a TAFKAL80ETC concert', 7, 12),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 14
+  end
+
+  def test_sulfuras_quality_should_not_decrease
+    items = [
+          Item.new('Sulfuras, Hand of Ragnaros', 7, 12),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 12
+  end
+
+  def test_sulfuras_sell_in_should_not_decrease
+    items = [
+          Item.new('Sulfuras, Hand of Ragnaros', 7, 12),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].sell_in == 7
+  end
+
+  def test_other_items_should_decrease_sell_in_by_1
+      items = [
+          Item.new('Any Other Item', 7, 12),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 11
+end
+
+  def test_other_items_should_decrease_quality_by_1
+      items = [
+          Item.new('Any Other Item', 7, 12),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].sell_in == 6
+  end
+
+  def test_conjured_quality_should_increment_by_2
+    items = [
+          Item.new('Conjured Mana Cake', 7, 12),
+        ]
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 10
+  end
+
+
 end
